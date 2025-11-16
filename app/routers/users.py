08ff9b0de134bc11df21a80b34990a0b4db7a954
ju4ndrc <jdramirez90@ucatolica.edu.co>
2025-11-15 23:15:25 -0500

    [!]Adapt users router for async engine

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,10 +1,6 @@
 import uuid
-from http.client import responses
 
-from fastapi import APIRouter, status, HTTPException, Query, Depends
-
-from app.auth.auth_router import admin_required
-from app.auth.hash import hash_password
+from fastapi import APIRouter, status, HTTPException
 
 from sqlmodel import select
 
@@ -16,17 +12,15 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED, )
 async def createUser(user_data: CreateUser, session: SessionDep):
-    hashed_pw = hash_password(user_data.password)
     user = User.model_validate({
         **user_data.model_dump(),
-        "password": hashed_pw
     })
     session.add(user)
     session.commit()
     session.refresh(user)
     return user
 @router.delete("/{user_id}", status_code=status.HTTP_200_OK)
-async def soft_delete_user(user_id: uuid.UUID, session: SessionDep, user : User = Depends(admin_required)):
+async def soft_delete_user(user_id: uuid.UUID, session: SessionDep):
     user_db = session.get(User, user_id)
     if not user_db:
         raise HTTPException(status_code=404, detail="User not found")
@@ -39,7 +33,7 @@ async def soft_delete_user(user_id: uuid.UUID, session: SessionDep, user : User 
     session.commit()
     return {"message": "User deactivated successfully", "user_id": str(user_id)}
 @router.patch("/reactivate/{user_id}", response_model=User)
-async def reactivate_user(user_id: uuid.UUID, session: SessionDep, user : User = Depends(admin_required)):
+async def reactivate_user(user_id: uuid.UUID, session: SessionDep):
     user_db = session.get(User, user_id)
     if not user_db:
         raise HTTPException(status_code=404, detail="User not found")
@@ -50,16 +44,16 @@ async def reactivate_user(user_id: uuid.UUID, session: SessionDep, user : User =
     session.refresh(user_db)
     return user_db
 @router.get("/",response_model=list[User])
-async def show_users(session:SessionDep, user : User = Depends(admin_required)):
+async def show_users(session:SessionDep):
     response = session.exec(select(User)).all()
     return response
 @router.get("/active", response_model=list[User])
-async def get_users(session: SessionDep, user : User = Depends(admin_required)):
+async def get_users(session: SessionDep):
     users = session.exec(select(User).where(User.is_active == True)).all()
     return users
 
 @router.patch("/updateUser/{user_id}",response_model=User,status_code= status.HTTP_201_CREATED)
-async def update_user( user_id: uuid.UUID, user_data:UpdateUser, session:SessionDep, user : User = Depends(admin_required) ):
+async def update_user( user_id: uuid.UUID, user_data:UpdateUser, session:SessionDep ):
     user_db = session.get(User, user_id)
 
     if not user_db:
