e9ace028c757b297704fa257edc8929eed5700a2
ju4ndrc <jdramirez90@ucatolica.edu.co>
2025-11-20 15:11:57 -0500

    [!]Fixin database for supabase bucket

--- models.py
+++ models.py
@@ -2,8 +2,7 @@ from sqlmodel import SQLModel, Field,Relationship
 from typing import Optional,List
 import datetime
 import uuid
-from pydantic import BaseModel, EmailStr, field_validator
-from db import  init_db
+from pydantic import EmailStr
 from enum import Enum
 
 class RoleEnum(str, Enum):
@@ -15,12 +14,13 @@ class UserBase(SQLModel):
     username: str | None = Field(description="User name")
     email: EmailStr = Field(default=None, unique = True)
     password: str
-    role: RoleEnum = Field(default=RoleEnum.CUSTOMER)
+    img : Optional[str] = Field(description="User image")
 
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
     date: datetime.datetime | None = Field(default_factory=datetime.datetime.now)
     is_active: bool = Field(default=True)
+    role: RoleEnum = Field(default=RoleEnum.CUSTOMER)
 
 
     products: List["Product"] = Relationship(back_populates="owner")
@@ -36,12 +36,12 @@ class ProductBase(SQLModel):
 class Product(ProductBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
     owner_id: uuid.UUID = Field(foreign_key="user.id")
-    img : str | None = None
+    img : Optional[str] = Field(description="Product image")
 
     owner: Optional["User"] = Relationship(back_populates="products")
-# -----------------------------
+
 # PEDIDOS
-# -----------------------------
+
 class OrderBase(SQLModel):
     total_amount: float
 
@@ -51,9 +51,9 @@ class Order(OrderBase, table=True):
 
     customer: Optional[User] = Relationship(back_populates="orders")
 
-# -----------------------------
+
 # CARRITO DE COMPRAS
-# -----------------------------
+
 class Cart(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
     customer_id: uuid.UUID = Field(foreign_key="user.id")
@@ -71,6 +71,8 @@ class CartItem(SQLModel, table=True):
 
     cart: Optional[Cart] = Relationship(back_populates="items")
     product: Optional[Product] = Relationship()
+
+
 class CreateUser(UserBase):
     pass
 class UpdateUser(UserBase):
